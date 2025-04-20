a231c5261afff0aaa7afce50fcdf84d11a816f64
yash07-code <yashsuryawanshi7799@gmail.com>
2025-04-20 22:44:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '	
+us-east-1_9xsCSzz8p', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4k3i28m2i78piimr89dagsf9qk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        clientSecret: 'q99nucm11a40dekqr7rucmfmbnbecchalev7vg45ge3rv6r1rrt'
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
